c9c8608e5b4a54c35fcdec8146d59d4466a88ba1
supernoir <kawohl.bentodesign@gmail.com>
2018-01-14 23:25:21 +0100

    using partials

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -63,8 +63,11 @@
                 margin-bottom: 30px;
             }
         </style>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
     </head>
     <body>
-        @yield('content')
+        @include('partials.header')
+        <div class="container">@yield('content')</div> 
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.master')
 
 @section('content')
-<h1>Some Content here</h1>
-<p>{{ 2 == 3 ? "Hello" : "Does not equal"  }}</p>
+<div class="row">
+  <div class="col-md-12">
+    <h1>Some Content here</h1>
+    <p>{{ 2 == 3 ? "Hello" : "Does not equal"  }}</p>
+  </div>
+</div>
 @endsection
