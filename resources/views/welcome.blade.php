c590bd79bd30182ae119876d430408e02dfc3666
supernoir <kawohl.bentodesign@gmail.com>
2018-01-14 23:30:14 +0100

    adds data and control structures

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,17 @@
 @section('content')
 <div class="row">
   <div class="col-md-12">
-    <h1>Some Content here</h1>
+    <h1>Control Structures</h1>
     <p>{{ 2 == 3 ? "Hello" : "Does not equal"  }}</p>
+    @if(false)
+    <p>This only displays if true</p>
+    @else
+    <p>This only displays if it is false</p>
+    @endif
+    <hr/>
+    @for($i = 0; $i < 5; $i++)
+      <p>{{ $i +1 }}. Iteration</p>
+    @endfor
   </div>
 </div>
 @endsection
