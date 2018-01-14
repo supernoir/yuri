070bcf9b0318ae0ecfc3f045c8c26d14024717a4
supernoir <kawohl.bentodesign@gmail.com>
2018-01-14 23:48:22 +0100

    using a facade to include stylesheet

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -65,6 +65,7 @@
         </style>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="{{URL::to('css/styles.css')}}">
     </head>
     <body>
         @include('partials.header')
