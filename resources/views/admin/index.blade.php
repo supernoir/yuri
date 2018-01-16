adad891e96cb39e3259f11940e63391e8a5b3e42
supernoir <kawohl.bentodesign@gmail.com>
2018-01-16 14:29:10 +0100

    adds routes and corresponding templates

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.admin')
+
+@section('content')
+  <div class="row">
+    <div class="col-md-12">
+      <a href="" class="btn btn-success">New Post</a>
+    </div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="col-md-12">
+      <p><strong>Learning Laravel</strong> <a href="">Edit</a></p>
+    </div>
+  </div>
+@endsection
---
+++ resources/views/other/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('content')
+  <div class="row">
+    <div class="col-md-12">
+      <p class="quote">About me</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <p>Lorem Ipsum</p>
+    </div>
+  </div>
+@endsection
